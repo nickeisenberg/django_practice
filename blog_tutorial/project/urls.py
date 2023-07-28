dc6e14be294e047ff2ea7cc3ce080e3d4ab03e96
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-28 10:54:19 -0700

    adding user images and dfault images

--- blog_tutorial/project/urls.py
+++ blog_tutorial/project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -35,3 +37,9 @@ urlpatterns = [
         name='logout'
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
