9247a52757d645d70fbdef8a138950b9dbc25f59
nickeisenberg <nickeisenberg@gmail.com>
2023-11-03 16:33:03 -0700

    added a delete post button

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room', views.createroom, name="create-room"),
     path('update-room/<str:pk>', views.updateroom, name="update-room"),
+    path('delete-room/<str:pk>', views.deleteroom, name="delete-room"),
 ]
 
