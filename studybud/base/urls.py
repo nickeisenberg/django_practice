cf719ee19cc96c1476482cd8b95556b50d8f8320
nickeisenberg <nickeisenberg@gmail.com>
2023-11-03 11:13:42 -0700

    adding models

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
 ]
+
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms = [
-    {'id': 1, 'name': "Lets learn python"},
-    {'id': 2, 'name': "desgin with me"},
-    {'id': 3, 'name': "front end devs"},
-]
+# rooms = [
+#     {'id': 1, 'name': "Lets learn python"},
+#     {'id': 2, 'name': "desgin with me"},
+#     {'id': 3, 'name': "front end devs"},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
