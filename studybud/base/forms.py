b4644840c3594a5e38be500de068f72c38fc3570
nickeisenberg <nickeisenberg@gmail.com>
2023-11-07 15:41:38 -0800

    update user works

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from .models import Room, User
+from django.contrib.auth.forms import UserCreationForm
 
 class RoomForm(ModelForm):
 
@@ -15,4 +15,10 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = [
+            'avatar',
+            'name',
+            'username', 
+            'email',
+            'bio'
+        ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
 from django.http import HttpResponse
 
@@ -189,11 +188,12 @@ def updateroom(request, pk):
 
 @login_required(login_url='/login')
 def updateuser(request):
+
     user = request.user
     form = UserForm(instance=user)
     
     if request.method == "POST":
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
