28e60f4cf052f89384ac211feccf1b9eedeb91c4
nickeisenberg <nickeisenberg@gmail.com>
2024-01-18 09:32:23 -0800

    removed the old view function and I removed the old url that called the old view function

--- progressbar/base/views.py
+++ progressbar/base/views.py
@@ -10,6 +10,7 @@ def home(request):
     context = {}
     return render(request, 'main.html', context)
 
+
 class Counter(View):
     def __init__(self):
         self.m = []
@@ -40,30 +41,3 @@ class Counter(View):
                             'total': 100000000
                         }
                     )
-
-
-def update_counter_old():
-    channel_layer = get_channel_layer()
-
-    then = time.time()
-    for x in range(100000000):
-        now = time.time()
-        if now - then > 1:
-            then = now
-            async_to_sync(channel_layer.group_send)(
-                'counter_group', 
-                {
-                    'type': 'counter.update',
-                    'current_count': x,
-                    'total': 100000000
-                }
-            )
-        elif x == 100000000 - 1:
-            async_to_sync(channel_layer.group_send)(
-                'counter_group', 
-                {
-                    'type': 'counter.update',
-                    'current_count': x + 1,
-                    'total': 100000000
-                }
-            )
