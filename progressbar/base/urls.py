fdbd6c9b2ef4d88ea1d40d20b5c6d052d859dad9
nickeisenberg <nickeisenberg@gmail.com>
2024-01-18 09:30:17 -0800

    turned the counter and updateer into a class based view to modularize the logic

--- progressbar/base/urls.py
+++ progressbar/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('counter/', views.counter, name='counter')
+    # path('counter/', views.counter, name='counter'),
+    path('counter/', views.Counter.as_view(), name='counter')
 ]
--- progressbar/base/views.py
+++ progressbar/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 import time
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
@@ -9,17 +10,39 @@ def home(request):
     context = {}
     return render(request, 'main.html', context)
 
+class Counter(View):
+    def __init__(self):
+        self.m = []
 
-def counter(request):
-    threading.Thread(
-        target=update_counter,
-        kwargs={'ls': range(100000000), "django_updater": True}
-    ).start()
-    context = {}
-    return render(request, 'base/counter.html', context)
+    def get(self, request, *args, **kwargs):
+        threading.Thread(
+            target=self._update_counter,
+            kwargs={'ls': range(100000000), "django_updater": True}
+        ).start()
+        context = {}
+        return render(request, 'base/counter.html', context)
+
+    def _update_counter(self, ls, django_updater):
+        then = time.time()
+        for i, l in enumerate(ls):
+            now = time.time()
+            self.m.append(l)
+
+            if django_updater:
+                if now - then > 1:
+                    then = now
+                    channel_layer = get_channel_layer()
+                    async_to_sync(channel_layer.group_send)(
+                        'counter_group', 
+                        {
+                            'type': 'counter.update',
+                            'current_count': i + 1,
+                            'total': 100000000
+                        }
+                    )
 
 
-def update_counter_():
+def update_counter_old():
     channel_layer = get_channel_layer()
 
     then = time.time()
@@ -44,26 +67,3 @@ def update_counter_():
                     'total': 100000000
                 }
             )
-
-
-def update_counter(ls, django_updater):
-
-    m = []
-
-    then = time.time()
-    for i, l in enumerate(ls):
-        now = time.time()
-        m.append(l)
-
-        if django_updater:
-            if now - then > 1:
-                then = now
-                channel_layer = get_channel_layer()
-                async_to_sync(channel_layer.group_send)(
-                    'counter_group', 
-                    {
-                        'type': 'counter.update',
-                        'current_count': i + 1,
-                        'total': 100000000
-                    }
-                )
